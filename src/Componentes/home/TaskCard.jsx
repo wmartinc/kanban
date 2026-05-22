0076c594b9d82036cdb949289bed6c6119fefea2
Martin <coxmartin877@gmail.com>
2026-05-22 13:04:44 -0600

    Se agrego el color de borde blanco para cuando se hace hover

--- src/Componentes/home/TaskCard.jsx
+++ src/Componentes/home/TaskCard.jsx
@@ -1,6 +1,6 @@
 const TaskCard = ({ information }) => {
   return (
-    <div className="p-5 rounded-xl cursor-pointer bg-neutral-800 border border-white/25">
+    <div className="p-5 rounded-xl cursor-pointer bg-neutral-800 border border-white/25 hover:border-white duration-500">
       <h1 className="text-white text-center uppercase">{information.titulo}</h1>
       <p className="text-white/50">{information.descripcion}</p>
     </div>  
