251d8d7b17ff40b38f065c6cacdbca6967e2dc93
Martin <coxmartin877@gmail.com>
2026-06-05 16:51:39 -0600

    Se le agrego a la boardcard la funcion para mostrar y determinar cual es el indice que esta marcado para que podamos tomar una accion sobre ese board, en navegation se le implementaron las opciones que tenmos para esos boards

--- src/Componentes/modals/ShowBoards.jsx
+++ src/Componentes/modals/ShowBoards.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react"
 import { useBoards } from "../../store/useBoards"
 import BoardsView from "../ui/shared/BoardCard"
 import Spinner from "../ui/shared/Spinner"
+import { Trash } from "lucide-react"
+import { SquarePen } from "lucide-react"
+import { ChevronRight } from "lucide-react"
+
+
+const actionClass = "cursor-pointer rounded-md text-white/50 transition duration-300"
 
 const ShowBoards = ({ data }) => {
   const boards = useBoards(state => state.boards)
-  
+  const [selectedBoard, setSelectedBoard] = useState(null)
+
+  const handleSelectBoard = (boardId) => {
+    setSelectedBoard(boardId)
+  }
+
+  const deleteBoard = () => {}
+  const updateBoard = () => {}
+
+  const goToBoard = () => {
+    
+  }
+
   return (
     <div className='absolute top-1/2 left-1/2 bg-neutral-950 w-[90%] max-w-200 translate-x-[-50%] translate-y-[-50%] rounded-lg
      border border-white/20 flex items-center  p-4 flex-col h-125'>
@@ -12,12 +31,24 @@ const ShowBoards = ({ data }) => {
       <div className="w-full py-4 h-full flex gap-2 flex-wrap justify-center overflow-y-scroll">
         {
           boards.length > 0 ? (boards.map(board => (
-            <BoardsView key={board.id} title={board.board_name} description={board.description} />
+            <BoardsView key={board.id} id={board.id} title={board.board_name} description={board.description} selectId={handleSelectBoard} />
           ))) : (<Spinner />)
         }
       </div>
+
+      {
+        selectedBoard &&
+        <div className="flex mt-2 flex-wrap p-2 gap-6 ml-auto">
+          <SquarePen className={`${actionClass} hover:text-white`} />
+          <Trash className={`${actionClass} hover:text-red-500/50`} />
+          <ChevronRight className={`${actionClass} hover:text-white`} />
+        </div>
+      }
+
     </div>
   )
 }
+
+
 
 export default ShowBoards;
--- src/Componentes/ui/shared/BoardCard.jsx
+++ src/Componentes/ui/shared/BoardCard.jsx
@@ -1,8 +1,15 @@
-const BoardCard = ({ title, description }) => {
+const BoardCard = ({ id, title, description, selectId }) => {
+
+  const selectBoard = () => {
+    if(selectId) {
+      selectId(id)
+    }
+  }
+  
   return (
     <div className="bg-neutral-900 border border-white/20 w-[95%] hover:border-white/40 transition duration-300 
-    sm:max-w-70 h-35 rounded-lg flex cursor-pointer items-center justify-center flex-col">
-      <h2 className="text-white text-lg font-medium">{title}</h2>
+    sm:max-w-70 h-35 rounded-lg flex cursor-pointer items-center justify-center flex-col" onClick={selectBoard}>
+      <h2 className="text-white text-lg font-medium" >{title}</h2>
       {
         description !== "" ?<div className="text-white/50">{description}</div> : <div className="text-white/50">No description</div>
       }
