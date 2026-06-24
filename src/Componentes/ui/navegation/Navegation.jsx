82f7df25ad1aec8adea3fc199ee971848e6a1e6e
Martin <coxmartin877@gmail.com>
2026-06-23 23:53:42 -0600

    Se implemento diseño responsivo de el menu

--- src/Componentes/ui/navegation/Navegation.jsx
+++ src/Componentes/ui/navegation/Navegation.jsx
@@ -6,6 +6,9 @@ import useGetBoards from '../../../hooks/useGetBoards';
 import { useEffect } from 'react';
 import { getBoards } from '../../../controllers/boards.controller';
 import { useBoards } from '../../../store/useBoards';
+import { Menu, User, Plus, Columns, Grid3x3, Star, Settings } from 'lucide-react';
+import { useState } from 'react';
+import { X } from 'lucide-react';
 
 const Navegation = () => {
   const { fetchBoards, loading, response } = useGetBoards();
@@ -17,28 +20,28 @@ const Navegation = () => {
       setBoards(response)
     }
   }, [loading, response])
-  
+
   const setModalBoard = () => {
     updateModalStatus(true, "addBoard")
   }
-  
+
   const getTotalBoards = async () => {
     updateModalStatus(true, "showBoards")
     fetchBoards();
-  } 
+  }
 
   const showFavoritesModal = () => {
     updateModalStatus(true, "favorites")
   }
 
   return (
-    <nav className="md:w-full w-[90%] py-2 md:px-4 flex items-center justify-between flex-col md:flex-row">
-      <div className="md:w-[30%] w-62.5 flex items-center justify-center">
+    <nav className="w-full py-2 md:px-4 flex items-center justify-between flex-col md:flex-row">
+      <div className="md:w-[30%] w-62.5 md:flex hidden items-center justify-center">
         <img src={logo} alt="logo" className='w-[50%] aspect-11/5  object-cover pointer-events-none user-select-none' />
       </div>
 
       <NavegationOptionsDesktop setModalBoard={setModalBoard} getTotalBoards={getTotalBoards} showFavoritesModal={showFavoritesModal} />
-      <NavegationOptionsMobile setModalBoard={setModalBoard} />
+      <NavegationOptionsMobile createBoard={setModalBoard} showBoards={getTotalBoards} showFavoriteBoards={showFavoritesModal} />
     </nav>
   )
 }
@@ -54,11 +57,37 @@ const NavegationOptionsDesktop = ({ setModalBoard, getTotalBoards, showFavorites
   )
 }
 
-const NavegationOptionsMobile = ({ setModalBoard }) => {
+const NavegationOptionsMobile = ({createBoard, showBoards, showFavoriteBoards }) => {
+  const [isShown, setIsShown] = useState(false)
+  const toggleMenu = () => {
+    setIsShown(!isShown)
+  }
+
+  const selectOption = (action) => {
+    action()
+    setIsShown(false)
+  }
+
+  const btn_menu = "transition[background] duration-500 p-2 w-full text-start text-sm flex items-center gap-2 cursor-pointer hover:bg-white/5"
+
   return (
     <div className='flex items-center gap-5 justify-between md:hidden w-full'>
-      <Searcher />
-      <Button variant="ghost" event={setModalBoard}>Create Board</Button>
+      <Menu className='stroke-white ml-auto cursor-pointer ' onClick={toggleMenu}/>
+      {/* Submenu */}
+      <div className={`text-white border rounded-[6px_0px_0px_6px] border-white/20 transition[position] duration-300 fixed top-0 w-1/2 ${isShown ? "right-0" : "-right-50"} h-full z-10 bg-[#0d0d0d]`}>
+        <div className='p-2 border-b border-white/20'>
+          <button className={btn_menu}><User size={18}/>My Account</button>
+        </div>
+        <button className={btn_menu} onClick={()=> selectOption(createBoard) } ><Plus size={18} />Create Board</button>
+        <button className={btn_menu} onClick={() => {}} ><Columns size={18} />Add a column</button>
+        <button className={btn_menu} onClick={() => selectOption(showBoards)} ><Grid3x3 size={18} />Select a new board</button>
+        <button className={btn_menu} onClick={() => selectOption(showFavoriteBoards)} ><Star size={18} />See Favorites</button>
+        <button className={btn_menu} onClick={() => {}} ><Settings size={18} />Settings</button>
+
+        <X className='absolute bottom-2 right-2' onClick={toggleMenu}/>
+      </div>
+
+
     </div>
   )
 }
